be55945de0869c3af5c645a8ad6075d8f9773d87
JP Duplessis <jp@vteam.com>
2025-09-01 18:59:27 -0700

    fix API deprication for .scaled()

--- example/lib/panel2_steps/panel2_steps_toolbar.dart
+++ example/lib/panel2_steps/panel2_steps_toolbar.dart
@@ -103,7 +103,7 @@ class _Panel2ToolbarState extends State<Panel2Toolbar>
               OutlinedButton(
                 onPressed: () {
                   widget.transformationController.value =
-                      widget.transformationController.value.scaled(1 / 1.5);
+                      widget.transformationController.value * (1 / 1.5);
                 },
                 child: const Text('-'),
               ),
@@ -111,7 +111,7 @@ class _Panel2ToolbarState extends State<Panel2Toolbar>
               OutlinedButton(
                 onPressed: () {
                   widget.transformationController.value =
-                      widget.transformationController.value.scaled(1.5);
+                      widget.transformationController.value * 1.5;
                 },
                 child: const Text('+'),
               ),
